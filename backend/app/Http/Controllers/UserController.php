d08d9bee1978e091634029971bd58322cae5b54d
Seda Özer <86311352+sedaozer4@users.noreply.github.com>
2023-04-04 07:36:13 +0300

    Update UserController.php
    UserController eklendi

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+        function register(Request $request){
+        $user = new User();
+        $user->name = $request->input("name");
+        $user->email = $request->input("email");
+        $user->password = Hash::make($request->input("password"));
+        $user->save();
+        return $user;
+    }
+
+    function login(Request $request){
+        $user= User::where('email', $request->first);
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return ["error"=>"Email veya şifre doğru değil"];
+        }
+        return $user;
+    }
 }
